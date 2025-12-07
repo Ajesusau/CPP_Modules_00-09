8cc3141604b574876c3095bfd5ec6a97b80fda47
anareval <anareval@student.42malaga.com>
2025-12-07 18:18:59 +0100

    Reemplazar la implementación de main en megaphone.cpp para procesar argumentos y mostrar texto en mayúsculas

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,13 +6,34 @@
 /*   By: anareval <anareval@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 17:06:25 by anareval          #+#    #+#             */
-/*   Updated: 2025/12/07 17:06:28 by anareval         ###   ########.fr       */
+/*   Updated: 2025/12/07 18:17:40 by anareval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!";
-  return 0;
+int	main(int argc, char **argv) {
+	int	i;
+	int	j;
+
+	i = 1;
+	if(argc >= 2)
+	{
+		while(argv[i])
+		{
+			j = 0;
+			while(argv[i][j])
+			{
+				std::cout << static_cast<char>(toupper(argv[i][j]));
+				j++;
+			}
+			if(i < argc - 1)
+				std::cout << " ";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
 }
