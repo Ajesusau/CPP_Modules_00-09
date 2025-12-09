36fb5eea1ced4579a8de224b4d19f45d9d39a0f9
anareval <anareval@student.42malaga.com>
2025-12-09 15:49:45 +0100

    Eliminar archivos innecesarios y actualizar Makefiles para incluir encabezados

--- CPP_00/ex01/main.cpp
+++ CPP_00/ex01/main.cpp
@@ -6,14 +6,76 @@
 /*   By: anareval <anareval@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/07 18:25:45 by anareval          #+#    #+#             */
-/*   Updated: 2025/12/07 18:27:05 by anareval         ###   ########.fr       */
+/*   Updated: 2025/12/09 15:40:29 by anareval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <unistd.h>
+
+void title_img()
+{
+	std::cout << "╔═══════════════════════════════════════════════════════════════════════════════════════════╗" << std::endl;
+	std::cout << "║                                                                                           ║" << std::endl;
+	std::cout << "║       ██████╗ ██╗  ██╗ ██████╗ ███╗   ██╗███████╗██████╗  ██████╗  ██████╗ ██╗  ██╗       ║" << std::endl;
+	std::cout << "║       ██╔══██╗██║  ██║██╔═══██╗████╗  ██║██╔════╝██╔══██╗██╔═══██╗██╔═══██╗██║ ██╔╝       ║" << std::endl;
+	std::cout << "║       ██████╔╝███████║██║   ██║██╔██╗ ██║█████╗  ██████╔╝██║   ██║██║   ██║█████╔╝        ║" << std::endl;
+	std::cout << "║       ██╔═══╝ ██╔══██║██║   ██║██║╚██╗██║██╔══╝  ██╔══██╗██║   ██║██║   ██║██╔═██╗        ║" << std::endl;
+	std::cout << "║       ██║     ██║  ██║╚██████╔╝██║ ╚████║███████╗██████╔╝╚██████╔╝╚██████╔╝██║  ██╗       ║" << std::endl;
+	std::cout << "║       ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝       ║" << std::endl;
+	std::cout << "║                                                                                           ║" << std::endl;
+	std::cout << "╚═══════════════════════════════════════════════════════════════════════════════════════════╝" << std::endl;
+}
+
+void clearScreen()
+{
+	std::cout << "\033[2J\033[2;1H";
+}
+
+void commands_menu()
+{
+	std::cout << "┌───────────────────────────────────────────────────────────────────────────────────────────┐" << std::endl;
+	std::cout << "│                                           OPTIONS                                         │" << std::endl;
+	std::cout << "├───────────────────────────────────────────────────────────────────────────────────────────┤" << std::endl;
+	std::cout << "│                                                                                           │" << std::endl;
+	std::cout << "│  ADD     → Add a new contact to your phonebook                                            │" << std::endl;
+	std::cout << "│            Parameters: First Name, Last Name, Nickname, Phone Number, and Darkest Secret  │" << std::endl;
+	std::cout << "│            Note: Maximum 8 contacts. New contacts will replace the oldest ones!           │" << std::endl;
+	std::cout << "│                                                                                           │" << std::endl;
+	std::cout << "│  SEARCH  → Display all contacts and view detailed information                             │" << std::endl;
+	std::cout << "│                                                                                           │" << std::endl;
+	std::cout << "│  EXIT    → Close the phonebook                                                            │" << std::endl;
+	std::cout << "│            All contacts will be permanently deleted !!!                                   │" << std::endl;
+	std::cout << "│                                                                                           │" << std::endl;
+	std::cout << "└───────────────────────────────────────────────────────────────────────────────────────────┘" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Select an option:" << std::endl;
+}
 
 int main()
 {
-	std::cout << "Hola" << std::endl; 
+	std::string option;
+
+	title_img();
+	sleep(2);
+	
+	while (option != "EXIT")
+	{
+		for (int i = 0; i < 30; i++)
+			std::cout << std::endl;
+		clearScreen();
+		commands_menu();
+		std::cin >> option;
+		if (std::cin.eof())
+			return (1);
+		else if (option == "ADD")
+		{
+			std::cout << "añado primo" << std::endl;
+		}
+		else if (option == "SEARCH")
+		{
+			std::cout << "busca loco" << std::endl;	
+		}
+	}
 	return 0;
 }
