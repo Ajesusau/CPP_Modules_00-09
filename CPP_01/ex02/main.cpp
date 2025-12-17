92ca3ee2730bfdc8e4ae49511e18967bf53e149f
anareval <anareval@student.42malaga.com>
2025-12-17 16:56:38 +0100

    Agregar implementación inicial de Makefile y main.cpp para la gestión de cadenas.

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main (void)
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string *stringPTR = &string;
+	std::string &stringREF = string;
+	std::cout << "-------------------MEMORY------------------" << std::endl;
+	std::cout << "String: " << &string << std::endl;
+	std::cout << "Pointer: " << stringPTR << std::endl;
+	std::cout << "Reference: " << &stringREF << std::endl;
+	std::cout << "-------------------VALUE-------------------" << std::endl;
+	std::cout << "String: " << string << std::endl;
+	std::cout << "Pointer: " << *stringPTR << std::endl;
+	std::cout << "Reference: " << stringREF << std::endl;
+
+	return 0;
+}
