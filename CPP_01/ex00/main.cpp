153540b51d18afde71b94b134e4cc5257369083d
anareval <anareval@student.42malaga.com>
2025-12-17 14:57:36 +0100

    Agregar implementación de la clase Zombie, incluyendo constructores, destructor y métodos, además de actualizar el Makefile y main.cpp para incluir nuevos archivos fuente.

---
+++ CPP_01/ex00/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "Zombie.hpp"
+
+int main (void)
+{
+	Zombie	*zombie;
+
+	std::cout << "-------------------TEST_1-------------------" << std::endl;
+	zombie = newZombie("Zombie_1");
+	zombie->announce();
+	delete zombie;
+	std::cout << "-------------------TEST_2-------------------" << std::endl;
+	randomChump("Zombie_2");
+
+	return 0;
+}
---
+++ CPP_01/ex00/src/Zombie.cpp
@@ -0,0 +1,22 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+	this->name = "default";
+}
+
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << "Zombie " << this->name << " has ben deleted." << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
