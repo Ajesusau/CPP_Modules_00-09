0b6fc970cec8dc53599e28b320ef032756153c94
anareval <anareval@student.42malaga.com>
2026-01-13 13:17:21 +0100

    Reorganizar la visibilidad de miembros en las clases HumanA y HumanB, y en la clase Harl para mejorar la legibilidad del código.

--- CPP_01/ex03/include/HumanA.hpp
+++ CPP_01/ex03/include/HumanA.hpp
@@ -7,13 +7,13 @@
 
 class HumanA
 {
-private:
-	std::string	_name;
-	Weapon&		_weapon;
-public:
-	HumanA(std::string name, Weapon& weapon);
-	~HumanA();
-	void	attack();
+	private:
+		std::string	_name;
+		Weapon&		_weapon;
+	public:
+		HumanA(std::string name, Weapon& weapon);
+		~HumanA();
+		void	attack();
 };
 
 #endif
--- CPP_01/ex06/include/Harl.hpp
+++ CPP_01/ex06/include/Harl.hpp
@@ -6,16 +6,16 @@
 
 class Harl
 {
-private:
-	void debug(void);
-	void info(void);
-	void warning(void);
-	void error(void);
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
 
-public:
-	Harl();
-	~Harl();
-	void complain(std::string level);
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
 };
 
 #endif
